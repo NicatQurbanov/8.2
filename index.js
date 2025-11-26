82fbee209d0e3a064cfb04750cba18d9b4eb8cf7
NicatQurbanov <nicatqur1307@gmail.com>
2025-11-26 19:37:28 +0400

    to be continued

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+<div class="sub-container">
+                <p class="sub-header">To Do</p>
+                <div class="task-container">
+                    <div class="tasks">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <div class="icon-container">
+                            <div><img src="Icons/justify.png" alt="justify"></div>
+                            <div>
+                                <div class="span-container">
+                                    <img src="Icons/checkbox.png" alt="checkbox"><span>4/8</span>
+                                </div>
+                                
+                            </div>
+                            <div>
+                                <div class="span-container">
+                                    <img src="Icons/attach.png" alt="attach"><span>2</span>
+                                </div></div>
+                            <div>
+                                <div class="span-container"><img src="Icons/comments.png" alt="comments"><span>4</span></div></div>
+                            <div><img src="Icons/man_1.png" alt="man"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>*/
