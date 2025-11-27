9b4ea780ff0b96e1347b3fdbb183a0780de40f3e
NicatQurbanov <nicatqur1307@gmail.com>
2025-11-27 13:00:48 +0400

    to be continued

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 
-
-                {/* <p class="sub-header">To Do</p>
-                <div class="task-container">
-                    <div class="tasks">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <div class="icon-container">
-                            <div><img src="Icons/justify.png" alt="justify"></div>
-                            <div>
-                                <div class="span-container">
-                                    <img src="Icons/checkbox.png" alt="checkbox"><span>4/8</span>
-                                </div>
-                                
-                            </div>
-                            <div>
-                                <div class="span-container">
-                                    <img src="Icons/attach.png" alt="attach"><span>2</span>
-                                </div></div>
-                            <div>
-                                <div class="span-container"><img src="Icons/comments.png" alt="comments"><span>4</span></div></div>
-                            <div><img src="Icons/man_1.png" alt="man"></div>
-                        </div>
-                    </div>
-                </div> */}
-            
-
 const btn = document.getElementById('btn');
 const main = document.getElementsByClassName('main-container')[0];
 
@@ -37,10 +12,7 @@ btn.addEventListener('click', () => {
     <input id="textInput" class="input-title" type="text" placeholder="${placeholdersArr[0]}">
     <button id="addCardBtn">Add card</button>`
     main.insertBefore(column, btn)
-})
-
- addCardBtn.addEventListener('click', () => {
-        console.log(addCardBtn)
+    addCardBtn.addEventListener('click', () => {
         let taskCont = document.createElement('div');
         taskCont.classList.add('task-container');
         taskCont.innerHTML = `
@@ -64,6 +36,8 @@ btn.addEventListener('click', () => {
                         </div>`
     column.insertBefore(taskCont, addCardBtn);
     })
+})
+ 
 
 
 
